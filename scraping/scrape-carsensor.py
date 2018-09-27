6f8ec383c455a3adae10039765dc0de0c0b33d4f
野口大輔 <>
2018-09-27 14:53:19 +0900

    csv完成

--- scraping/scrape-carsensor.py
+++ scraping/scrape-carsensor.py
@@ -12,9 +12,7 @@ options.add_argument('--headless')
 browser = webdriver.Chrome(chrome_options=options)
 
 url = "https://www.carsensor.net/usedcar/search.php?SKIND=1"
-makers_list = [] #メーカーを保存するリスト
-makers_code = [] #メーカーのコードを保存するリスト
-cartype_list = [] #車種を保存するリスト
+export_list = [] #出力を保存するリスト
 
 # 暗黙的な待機を最大3秒行う(サーバーの負担軽減)
 browser.implicitly_wait(3)
@@ -26,12 +24,12 @@ html = browser.page_source
 browser.find_element_by_id('shashuAnc').click()
 # HTMLを解析する --- (※3)
 soup = BeautifulSoup(html, "html.parser")
-# メーカー名を取得
+# メーカー名/車種名を取得
 makers = soup.find_all("a", "js_makerMenu", href="#")
 cartypes = soup.find_all("a", class_="js_makerMenu", href=re.compile(r"javascript:void"))
-makers_count = 0
-# 除外する項目
-skip_list = ["こだわらない","国産車その他","輸入車その他"]
+row_index = 0
+
+skip_list = ["こだわらない","国産車その他","輸入車その他"] # 除外する項目
 
 r = re.compile("[A-Z]{2}") #任意の連続する２つのアルファベット == maker_code
 
@@ -44,38 +42,49 @@ for maker in makers:
   # 正規表現で余分なものを取る
   maker = re.sub(r'\(\d*\)', "", maker) #(数字)
   maker = re.sub(r'\s', "", maker) #空白
+
   #除外項目をスキップ
   if not maker in skip_list:
-    makers_list.append([])
-    makers_code.append([])
+    export_list.append([])
 
-    makers_list[makers_count].append(str(makers_count+1))
-    makers_list[makers_count].append(maker)
-    makers_list[makers_count].append(maker_code[0])
+    export_list[row_index].append(str(row_index+1))
+    export_list[row_index].append(maker)
+    export_list[row_index].append(maker_code[0])
 
-    makers_code[makers_count].append(maker_code[0])
+    row_index += 1
 
-    makers_count += 1
-
-    #車種名を取得する
-    # print(makers_count," ",maker_code[0])
 
 for cartype in cartypes:
+  #メーカーコードの取得
+  maker_code = r.findall("%s" %cartype)
+
+  # テキスト部分の抽出
   cartype = cartype.text
+
+  # 正規表現で余分なものを取る
   cartype = re.sub(r'\(\d*\)', "", cartype) #(数字)
-  if not (cartype in skip_list or cartype in makers_list):
-    print(cartype)
+
+  if cartype == "その他": continue
+
+  export_list.append([])
+  export_list[row_index].append(str(row_index+1))
+  export_list[row_index].append(cartype)
+  export_list[row_index].append(maker_code[2])
+
+  for maker in export_list:
+    #もしメーカーコードが一致すればarticle_tag_id(maker[0])を付与
+    if maker_code[2] == maker[2]:
+      export_list[row_index].append(maker[0])
+      break
+
+  row_index += 1
 
 
+#csvに書き込み
+with open('article_tags.csv', 'w') as f:
+    writer = csv.writer(f, lineterminator='\n')
+    writer.writerows(export_list)
 
-
-# #csvに書き込み(メーカー名)
-# with open('index_makers.csv', 'w') as f:
-#     writer = csv.writer(f, lineterminator='\n')
-#     writer.writerows(makers_list)
-
-# 画面をキャプチャしてファイルに保存 --- (※4)
-    #browser.save_screenshot("website.png")
 
 # ブラウザを終了
 browser.quit()

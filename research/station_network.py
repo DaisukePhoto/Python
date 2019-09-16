3a0c373f6e8c2ebf0ff026386f321b1bc1b71bb0
野口大輔 <einstein.mastermind11235@gmail.com>
2019-09-17 05:38:05 +0900

    描画完成

--- research/station_network.py
+++ research/station_network.py
@@ -12,15 +12,14 @@ f = csv.reader(csv_file)
 pos={}
 for i, row in enumerate(f):
   # 頂点を設定する．引数はid．
-  G.add_node("node%d" % i)
+  G.add_node("st_%s" % row[2])
   # 座標を設定する．indexがid，代入している値が座標．
-  pos["node%d" % i] = (int(row[0]), int(row[1]))
+  pos["st_%s" % row[2]] = (int(row[1]), int(row[0]))
 
 csv_file = open("station_links.csv", "r", encoding="utf_8", errors="", newline="")
 f = csv.reader(csv_file)
 for row in f:
-  print("node%d" % int(row[0]), " node%d" % int(row[1]))
-  G.add_edge("node%d" % int(row[0]), "node%d" % int(row[1]))
+  G.add_edge("st_%s" % row[0], "st_%s" % row[1])
 
 
 
@@ -28,6 +27,7 @@ for row in f:
 
 # グラフオブジェクト（点と辺）に座標を関連付けて描画
 nx.draw(G, pos, node_size=1)
+nx.draw_networkx_labels(G, pos, font_size=7)
 
 #図を描画
 plt.show()

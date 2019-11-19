44b7dd10385a2219c4ccc2181b3c8f4de926bb57
野口大輔 <einstein.mastermind11235@gmail.com>
2019-11-19 16:00:00 +0900

    ファイル移動/描画修正

--- research/station_network.py
+++ research/station_network.py
@@ -7,7 +7,7 @@ import numpy
 # G = nx.read_edgelist('station_links.csv', nodetype=int)
 G = nx.Graph()
 
-csv_file = open("station_pos.csv", "r", encoding="utf_8", errors="", newline="")
+csv_file = open("data/station_pos.csv", "r", encoding="utf_8", errors="", newline="")
 f = csv.reader(csv_file)
 pos={}
 for i, row in enumerate(f):
@@ -16,7 +16,7 @@ for i, row in enumerate(f):
   # 座標を設定する．indexがid，代入している値が座標．
   pos["st_%s" % row[2]] = (int(row[1]), int(row[0]))
 
-csv_file = open("station_links.csv", "r", encoding="utf_8", errors="", newline="")
+csv_file = open("data/station_links.csv", "r", encoding="utf_8", errors="", newline="")
 f = csv.reader(csv_file)
 for row in f:
   G.add_edge("st_%s" % row[0], "st_%s" % row[1])

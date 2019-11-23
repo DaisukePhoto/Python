fd5119734306817b460f2c6aac593c0728eda3df
野口大輔 <einstein.mastermind11235@gmail.com>
2019-11-24 05:08:19 +0900

    修正

--- research/plt_totalization_15min.py
+++ research/plt_totalization_15min.py
@@ -12,14 +12,6 @@ else:
 df_header = pd.read_csv('data/aggregate_people_15min.csv')
 data = df_header[ (st_id - 1) : st_id ]
 
-# タイトルとラベルの設定
-plt.title('station id : ' + str(st_id))
-plt.xlabel('time range')
-plt.ylabel(('number of people'))
-
-# 軸の範囲を設定
-plt.ylim(0, 150000)
-
 left = ['~6', '6', '615', '630', '645', '7', '715', '730', 
         '745', '8', '815', '830', '845', '9', '915', '930', '945', 
         '10', '1015', '1030', '1045', '11', '1115', '1130', '1145']
@@ -28,6 +20,15 @@ height = np.array([int(data.bef0600), int(data.af0600), int(data.af0615), int(da
                 int(data.af0745), int(data.af0800), int(data.af0815), int(data.af0830), int(data.af0845), int(data.af0900), int(data.af0915), int(data.af0930),
                 int(data.af0945), int(data.af1000), int(data.af1015), int(data.af1030), int(data.af1045), int(data.af1100), int(data.af1115), int(data.af1130), int(data.af1145)])
 
+plt.figure(figsize=(12, 10), dpi=80)
 plt.bar(left, height)
+
+# タイトルとラベルの設定
+plt.title('station id : ' + str(st_id))
+plt.xlabel('time range')
+plt.ylabel(('number of people'))
+
+# 軸の範囲を設定
+plt.ylim(0, 100000)
 
 plt.show()

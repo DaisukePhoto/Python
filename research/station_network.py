be2de87583a27afa88de11aaa28f3c6cac6076a8
野口大輔 <einstein.mastermind11235@gmail.com>
2019-12-01 18:14:10 +0900

    ネットワーク連続出力

--- research/station_network.py
+++ research/station_network.py
@@ -29,16 +29,30 @@ f = csv.reader(csv_file)
 for row in f:
   G.add_edge("%s" % row[0], "%s" % row[1])
 
+for i in range(2, 62):
+  plt.figure(figsize=(18, 12), dpi=150)
+  csv_file = open("data/people_5min_percentage.csv", "r", encoding="utf_8", errors="", newline="")
+  f = csv.reader(csv_file)
 
-plt.figure(figsize=(16, 8), dpi=100)
+  over_50 = []
+  over_65 = []
+  over_80 = []
+  for row in f:
+    if int(row[i]) >= 80:
+      over_80.append(str(row[1]))
+    elif int(row[i]) >= 65:
+      over_65.append(str(row[1]))
+    elif int(row[i]) >= 50:
+      over_50.append(str(row[1]))
 
-# グラフオブジェクト（点と辺）に座標を関連付けて描画
-nx.draw(G, pos, node_size=8, node_color='yellowgreen', edge_color='white', weight=1)
-nx.draw(G, pos, nodelist=['8', '108', '118', '105'], node_size=20, node_color='yellow')
-nx.draw(G, pos, nodelist=['1'], node_size=25, node_color='orange')
-nx.draw(G, pos, nodelist=['38', '39', '40'], node_size=30, node_color='red')
 
-#図を描画
-plt.axis('off')
-plt.savefig('station_network.png')
-plt.show()
+  # グラフオブジェクト（点と辺）に座標を関連付けて描画
+  nx.draw(G, pos, node_size=8, node_color='yellowgreen', edge_color='white', weight=1)
+  nx.draw(G, pos, nodelist=over_50, node_size=35, node_color='yellow')
+  nx.draw(G, pos, nodelist=over_65, node_size=40, node_color='orange')
+  nx.draw(G, pos, nodelist=over_80, node_size=50, node_color='red')
+
+  #図を描画
+  plt.axis('off')
+  plt.savefig(f"figs_net/station_network_{i-1}.png")
+  plt.close()

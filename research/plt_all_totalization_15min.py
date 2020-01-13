6d9cecc7fc5db62178477a297db84a46616f989d
野口大輔 <einstein.mastermind11235@gmail.com>
2020-01-14 02:58:51 +0900

    最終段階

--- research/plt_all_totalization_15min.py
+++ research/plt_all_totalization_15min.py
@@ -1,3 +1,5 @@
+# coding: UTF-8
+
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
@@ -5,6 +7,7 @@ import numpy as np
 import sys
 
 df_header = pd.read_csv('data/aggregate_people_15min.csv')
+print('data/aggregate_people_15min.csv')
 
 for i in range(1, 650):
   st_id = i
@@ -41,12 +44,7 @@ for i in range(1, 650):
 
   plt.grid(which='major', axis='y', color='gray',linestyle='-')
 
-  # pdfファイルの初期化
-  pp = PdfPages(f"figs/15min/{st_id}_{st_name}.pdf")
-
   # figureをセーブする
-  pp.savefig()
-
+  plt.savefig(f"figs/15min/normal/{st_id}_{st_name}.png")
   # ファイルをクローズする。
-  pp.close()
   plt.close()
